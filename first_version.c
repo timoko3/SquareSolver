0944898c5a0048337f9ed365cc592fdd8627edd6
timoko3 <104276135+timoko3@users.noreply.github.com>
2025-08-22 16:16:51 +0300

    corrected mentor comments

--- first_version.c
+++ first_version.c
@@ -3,62 +3,56 @@
 #include <stdlib.h>
 #include <math.h>
 #define INFINITY_OF_ROOTS -1
-#define NO_VALID_NUMBERS 3
+#define NO_VALID_NUMBERS -2
+#define TWO_ROOTS 2
+#define ONE_ROOT 1
+#define NO_ANY_ROOTS 0
+#define INFELICITY 0.000001
 
 // прототипы функций
 int solveQuadEqua(double a, double b, double c, double *x1, double *x2);
 void finalOutput(double * x1, double * x2, int nRoots);
 void clearBuffer();
+int solveLinear(int a, int b, int c, double *x1);
 
 int main(void){
+
     // Инициализация переменных
     double a = 0,b = 0,c = 0;
     double x1 = 0, x2 = 0;
+
     // Ввод парметров квадратного уравнения
     printf("Введите парметры приведенного квадратного уравнения ax^2+bx+c:\n");
     while(scanf("%lf %lf %lf", &a, &b, &c) != 3){
         printf("Необходимо вводить парметры в виде: a b c\n");
-        // очистка буфера
-        clearBuffer();
+        clearBuffer();// очистка буфера
     }
-    // *******************************
+
     int nRoots = solveQuadEqua(a,b,c, &x1, &x2); // Вызов основной функции
-    finalOutput(&x1, &x2, nRoots);
+    finalOutput(&x1, &x2, nRoots); // вызов функции вывода
     return 0;
 }
 int solveQuadEqua(double a, double b, double c,
                   double * x1, double * x2){
-    double D = b * b - 4 * a * c;
+    double D = b * b - 4 * a * c; //вычисление дискриминанта
+
     // условие отстуствия дейстивтельных корней
     if(D < 0){
         return NO_VALID_NUMBERS;
     }
-    else if ((D - 0)  < 0.000001){
+    else if ((D - 0)  < INFELICITY){
         *x1 = (-b) / (2 * a);
-        return 10;
+        return ONE_ROOT;
     }
     else{
-        // решение неполного квадратного уравнения
-        if(a == 0){
-            if(b == 0){
-                if(c == 0){
-                    return INFINITY_OF_ROOTS;
-                }
-                else{
-                    return 0;
-                }
-            }
-            else{
-                *x1 = c;
-                return 1;
-            }
+        // обработка случая a = 0
+        if((a - 0) < INFELICITY){
+            return solveLinear(a,b,c,x1);
         }
-        // решение квадратного уравнения с a != 0
         else{
-
             *x1 = (-b + sqrt(D)) / (2 * a);
             *x2 = (-b - sqrt(D)) / (2 * a);
-            return 2;
+            return TWO_ROOTS;
         }
     }
 
@@ -69,19 +63,45 @@ int solveQuadEqua(double a, double b, double c,
 void finalOutput(double * x1, double*x2,
                  int nRoots){
     switch(nRoots){
-        case 0: printf("Данное уравнения не имеет решений\n");break;
-        case 1:
+        case NO_ANY_ROOTS:
+            printf("Данное уравнения не имеет решений\n");
+            break;
+        case ONE_ROOT:
             printf("Данное уравнение имеет один корень x1 = %lf\n", *x1);
             break;
-        case 2: printf("Данное уравнение имеет два корня x1 = %lf, x2 = %lf\n", *x1,*x2);break;
-        case NO_VALID_NUMBERS: printf("Данное уравнение не имеет решений в действительных числах");break;
-        case INFINITY_OF_ROOTS: printf("Данное уравнение имеет бесконечное количество решений\n");break;
-        default: exit(1);
+        case TWO_ROOTS:
+            printf("Данное уравнение имеет два корня x1 = %lf, x2 = %lf\n", *x1,*x2);
+            break;
+        case NO_VALID_NUMBERS:
+            printf("Данное уравнение не имеет решений в действительных числах");
+            break;
+        case INFINITY_OF_ROOTS:
+            printf("Данное уравнение имеет бесконечное количество решений\n");
+            break;
+        default: printf("Произошла ошибка, кол-во корней — %d\n", nRoots);
     }
 }
+// очистка буфера
 void clearBuffer(){
     while(getchar() != '\n'){
         continue;
     }
 }
+int solveLinear(int a, int b, int c, double * x1){
+    // (x-0) < INFELICITY — проверка x на равенство 0
+    if((b - 0) < INFELICITY){
+        if((c - 0) < INFELICITY){
+            return INFINITY_OF_ROOTS;
+        }
+        else{
+            return NO_ANY_ROOTS;
+        }
+    }
+    else{
+        *x1 = c;
+        return ONE_ROOT;
+    }
+}
+
+
 
