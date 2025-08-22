f029c244002db17d66c77cd00728ee255479705d
timoko3 <104276135+timoko3@users.noreply.github.com>
2025-08-22 16:10:15 +0300

    Add files via upload
    corrected mentor comments1

---
+++ main_quad.c
@@ -0,0 +1,107 @@
+//main_quad.c — Решение квадратного уравнения
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#define INFINITY_OF_ROOTS -1
+#define NO_VALID_NUMBERS -2
+#define TWO_ROOTS 2
+#define ONE_ROOT 1
+#define NO_ANY_ROOTS 0
+#define INFELICITY 0.000001
+
+// прототипы функций
+int solveQuadEqua(double a, double b, double c, double *x1, double *x2);
+void finalOutput(double * x1, double * x2, int nRoots);
+void clearBuffer();
+int solveLinear(int a, int b, int c, double *x1);
+
+int main(void){
+
+    // Инициализация переменных
+    double a = 0,b = 0,c = 0;
+    double x1 = 0, x2 = 0;
+
+    // Ввод парметров квадратного уравнения
+    printf("Введите парметры приведенного квадратного уравнения ax^2+bx+c:\n");
+    while(scanf("%lf %lf %lf", &a, &b, &c) != 3){
+        printf("Необходимо вводить парметры в виде: a b c\n");
+        clearBuffer();// очистка буфера
+    }
+
+    int nRoots = solveQuadEqua(a,b,c, &x1, &x2); // Вызов основной функции
+    finalOutput(&x1, &x2, nRoots); // вызов функции вывода
+    return 0;
+}
+int solveQuadEqua(double a, double b, double c,
+                  double * x1, double * x2){
+    double D = b * b - 4 * a * c; //вычисление дискриминанта
+
+    // условие отстуствия дейстивтельных корней
+    if(D < 0){
+        return NO_VALID_NUMBERS;
+    }
+    else if ((D - 0)  < INFELICITY){
+        *x1 = (-b) / (2 * a);
+        return ONE_ROOT;
+    }
+    else{
+        // обработка случая a = 0
+        if((a - 0) < INFELICITY){
+            return solveLinear(a,b,c,x1);
+        }
+        else{
+            *x1 = (-b + sqrt(D)) / (2 * a);
+            *x2 = (-b - sqrt(D)) / (2 * a);
+            return TWO_ROOTS;
+        }
+    }
+
+
+}
+
+// функция вывода в зависимости от кол-ва корней уравнения
+void finalOutput(double * x1, double*x2,
+                 int nRoots){
+    switch(nRoots){
+        case NO_ANY_ROOTS:
+            printf("Данное уравнения не имеет решений\n");
+            break;
+        case ONE_ROOT:
+            printf("Данное уравнение имеет один корень x1 = %lf\n", *x1);
+            break;
+        case TWO_ROOTS:
+            printf("Данное уравнение имеет два корня x1 = %lf, x2 = %lf\n", *x1,*x2);
+            break;
+        case NO_VALID_NUMBERS:
+            printf("Данное уравнение не имеет решений в действительных числах");
+            break;
+        case INFINITY_OF_ROOTS:
+            printf("Данное уравнение имеет бесконечное количество решений\n");
+            break;
+        default: printf("Произошла ошибка, кол-во корней — %d\n", nRoots);
+    }
+}
+// очистка буфера
+void clearBuffer(){
+    while(getchar() != '\n'){
+        continue;
+    }
+}
+int solveLinear(int a, int b, int c, double * x1){
+    // (x-0) < INFELICITY — проверка x на равенство 0
+    if((b - 0) < INFELICITY){
+        if((c - 0) < INFELICITY){
+            return INFINITY_OF_ROOTS;
+        }
+        else{
+            return NO_ANY_ROOTS;
+        }
+    }
+    else{
+        *x1 = c;
+        return ONE_ROOT;
+    }
+}
+
+
+
