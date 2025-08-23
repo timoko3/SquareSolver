24a2f852e9736b007b8c3dc0361311e73dc87813
timoko3 <104276135+timoko3@users.noreply.github.com>
2025-08-23 12:58:40 +0300

    added file division

---
+++ computing.c
@@ -0,0 +1,45 @@
+#include "funcDefinitions.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <stdbool.h>
+int solveQuadEqua(double a, double b, double c,
+                  double* x1, double* x2){
+
+    // линейный случай
+    if(isZero(a)){
+        return solveLinear(b, c, x1);
+    }
+
+    double D = b * b - 4 * a * c; //вычисление дискриминанта
+
+    // условие отстуствия дейстивтельных корней
+    if(D < 0){
+        return NO_VALID_NUMBERS;
+    }
+    else if (isZero(D)){
+        *x1 = (-b) / (2 * a);
+        return ONE_ROOT;
+    }
+    else{
+        *x1 = (-b + sqrt(D)) / (2 * a);
+        *x2 = (-b - sqrt(D)) / (2 * a);
+        return TWO_ROOTS;
+    }
+}
+
+// решение линейного варианта квадратного уравнения
+int solveLinear(double b, double c, double* x){
+    if(isZero(b)){
+        if(isZero(c)){
+            return INFINITY_OF_ROOTS;
+        }
+        else{
+            return NO_ANY_ROOTS;
+        }
+    }
+    else{
+        *x = c;
+        return ONE_ROOT;
+    }
+}
---
+++ sidefunc.c
@@ -0,0 +1,21 @@
+#include "funcDefinitions.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <stdbool.h>
+// проверка double на равенство нулю
+bool isZero(double number){
+    if(fabs(number) < INFELICITY){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+// очистка буфера
+void clearBuffer(){
+    while(getchar() != '\n'){
+        continue;
+    }
+}
