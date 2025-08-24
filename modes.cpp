7f333b380c0c0707ae6292961f05c84bc7d2eb1e
timoko <freepower27@gmail.com>
2025-08-24 23:37:43 +0300

    placed parametres and roots variables in structure

--- modes.cpp
+++ modes.cpp
@@ -3,15 +3,28 @@
 #include "solve.h"
 
 const char* WORK_IN_PROGRESS = "В разработке\n";
+struct coefficent 
+{
+    double a;
+    double b;
+    double c;
+};
+struct root
+{
+    double x1;
+    double x2;
+};
+
 
 int userEquations(){
-    double a = 0,b = 0,c = 0;
-    double x1 = 0, x2 = 0;
-    if(get_coefficents(&a, &b, &c) == RETURN_TO_TOP_MENU){
+    coefficent coefficents = {0,0,0};
+    root roots = {0,0};
+    
+    if(get_coefficents(&coefficents.a, &coefficents.b, &coefficents.c) == RETURN_TO_TOP_MENU){
         return RETURN_TO_TOP_MENU;
     }
-    numRoots curOutput = solveQuadEqua(a,b,c, &x1, &x2); // Вызов основной функции
-    printFinalOutput(&x1, &x2, curOutput); // вызов функции вывода
+    numRoots curOutput = solveQuadEqua(coefficents.a,coefficents.b,coefficents.c, &roots.x1, &roots.x2); // Вызов основной функции
+    printFinalOutput(&roots.x1, &roots.x2, curOutput); // вызов функции вывода
     return 0;
 }
 
